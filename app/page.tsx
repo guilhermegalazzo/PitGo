11ae6b1473588221aef2cb156ea8edfd7b309f6c
Guilherme Galazzo <guilhermegalazzo@gmail.com>
2026-02-10 16:00:48 -0500

    fix: make homepage dynamic to support auth cookies

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,11 @@
 import { getShops } from "@/app/actions/shops";
 import { HomeClient } from "@/components/home/HomeClient";
 
-export const revalidate = 3600; // Cache for 1 hour
+export const dynamic = "force-dynamic";
 
 export default async function Page() {
   // Fetch initial data on the server
   const initialShops = await getShops("all");
-  
+
   return <HomeClient initialShops={initialShops || []} />;
 }
